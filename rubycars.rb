5fbe1c2452a23bd746840b761f67cca75eb905a9
Matt Brooks <mattbrooks2010@gmail.com>
2020-07-09 20:08:17 -0500

    github probs

--- rubycars.rb
+++ rubycars.rb
@@ -4,10 +4,10 @@ def question(call_value)
 end
 
 
-years = question("Year")
+year = question("Year")
 make = question("Make")
 model = question("Model")
 mileage = question("Mileage")
 price = question("Price")
 
-puts "#{years.chomp} #{make.chomp} #{model.chomp} #{mileage.chomp} $#{price.chomp}"
+puts "#{year.chomp} #{make.chomp} #{model.chomp} #{mileage.chomp} $#{price.chomp}"
