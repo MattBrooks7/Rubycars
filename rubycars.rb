95c6d91707262dbbc8708eeeef5e45a3ba143e7c
Matt Brooks <mattbrooks2010@gmail.com>
2020-07-13 22:28:32 -0500

    I'm not getting it.

--- rubycars.rb
+++ rubycars.rb
@@ -1,13 +1,34 @@
-def question(call_value)
-    puts "What #{call_value} is your car?"
-    gets 
+class Vehicle
+    attr_accessor :call_vallue, :year, :make, :model, :mileage, :price
+
+    def initialize(year, make, model, mileage, price)
+        @year = year
+        @make = make
+        @model = model
+        @mileage = mileage
+        @price= price
+        @call_vallue = @year, @make, @model, @mileage, @price
+    end 
+
 end
 
+def question(call_vallue)
+    puts"What #{@call_vallue} is your car?"
+    gets
+    end
+#class Vehicle
+#   attr_accessor :year, :make, :model, :mileage, :price
+#end
 
-year = question("Year")
-make = question("Make")
-model = question("Model")
-mileage = question("Mileage")
-price = question("Price")
+#vehicle1 = Vehicle.new(2006, "Acura", "RSX", 160000, 10000)
 
-puts "#{year.chomp} #{make.chomp} #{model.chomp} #{mileage.chomp} $#{price.chomp}"
+puts "What year is your car?"
+gets
+puts "What make is your car?"
+gets
+puts "What model is your car?"
+gets
+puts "What is your car's mileage?"
+gets
+puts "What is your car's price?"
+gets
