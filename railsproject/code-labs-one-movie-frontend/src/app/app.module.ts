6057c857eb4a3695a99841fe18889d7a649e362a
Matt Brooks <mattbrooks2010@gmail.com>
2020-09-14 14:55:13 -0500

    figured it out...i think

--- railsproject/code-labs-one-movie-frontend/src/app/app.module.ts
+++ railsproject/code-labs-one-movie-frontend/src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import {
+  faSpinner, faAngleLeft, faAngleRight, faPlus, faStar, faStarHalfAlt, faArrowRight, faUpload, faVideo,
+  faEdit, faPen, faTrashAlt, faSearch, faUser, faKey, faEye, faEyeSlash, faSignInAlt, faSignoutAlt, faUserPlus, faCircle
+} from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
   declarations: [
@@ -23,6 +27,9 @@ export class AppModule {
   constructor(
     private library: FaIconLibrary
   ) {
-    this.library.addIcons()
+    this.library.addIcons(
+      faSpinner, faAngleLeft, faAngleRight, faPlus, faStar, faStarHalfAlt, faArrowRight, faUpload, faVideo,
+  faEdit, faPen, faTrashAlt, faSearch, faUser, faKey, faEye, faEyeSlash, faSignInAlt, faSignoutAlt, faUserPlus, faCircle      
+    )
   }
 }
