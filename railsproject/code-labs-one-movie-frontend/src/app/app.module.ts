294ad3e8b39e083dd25214048bce750e6a84a8bc
Matt Brooks <mattbrooks2010@gmail.com>
2020-09-14 08:35:59 -0500

    got stuck

--- railsproject/code-labs-one-movie-frontend/src/app/app.module.ts
+++ railsproject/code-labs-one-movie-frontend/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(
+    private library: FaIconLibrary
+  ) {
+    this.library.addIcons()
+  }
+}
